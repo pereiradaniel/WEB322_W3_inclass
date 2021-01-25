69b3dea28cf0a49491840355cd827d2f632654db
pereiradaniel <danielpaulpereira@protonmail.com>
2021-01-25 14:09:05 -0500

    Fix setName function to setStudentName()

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 var student_info = {
   name: "Haytham",
   age: 32,
-  setName: function(n) { this.name = n;}
+  setStudentName: function(n) { this.name = n;}
 };
 
 console.log(student_info.name);
 console.log(student_info["name"]);
-
 student_info.setStudentName("Maria");
-
 console.log(student_info.name);
+
+var std1 = Object.create(student_info);
+std1.setStudentName("May");
+console.log(std1.age);
+console.log(std1.name);
