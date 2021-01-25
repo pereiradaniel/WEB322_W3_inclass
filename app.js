56d59f8d2f81d464c429dc937b1253fe3d908258
pereiradaniel <danielpaulpereira@protonmail.com>
2021-01-25 14:18:28 -0500

    Add new examples.

--- app.js
+++ app.js
@@ -6,18 +6,27 @@ var student_info = {
   getAge: function() { return this.age; }
 };
 
-// console.log(student_info["name"]);
-
+// Two ways of accessing name:
+console.log(student_info["name"]);
 console.log(student_info.name);
 
+// Change name to Maria:
 student_info.setStudentName("Maria");
 console.log(student_info.name);
 
+// Create a new object, copying data from student_info:
 var std1 = Object.create(student_info);
+// Change name to May:
 std1.setStudentName("May");
+// Display age and name:
 console.log(std1.age);
 console.log(std1.name);
 
+// Change age to 44:
 std1.age=44;
-console.log(std1.age);
+// Use this.getAge() to display name:
 console.log(std1.getAge());
+
+// Copy into a new object from student_info:
+var std2 = Object.create(student_info);
+console.log(std2.age);
