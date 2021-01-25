7eed850b342454aeb865d25b1f248965fc9bc601
pereiradaniel <danielpaulpereira@protonmail.com>
2021-01-25 14:12:35 -0500

    Add getAge function.

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 var student_info = {
   name: "Haytham",
   age: 32,
-  setStudentName: function(n) { this.name = n;}
+  setStudentName: function(n) { this.name = n;},
+  getAge: function() { return this.age; }
 };
 
+// console.log(student_info["name"]);
+
 console.log(student_info.name);
-console.log(student_info["name"]);
+
 student_info.setStudentName("Maria");
 console.log(student_info.name);
 
@@ -14,3 +17,7 @@ var std1 = Object.create(student_info);
 std1.setStudentName("May");
 console.log(std1.age);
 console.log(std1.name);
+
+std1.age=44;
+console.log(std1.age);
+console.log(std1.getAge());
